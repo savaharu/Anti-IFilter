8b35b2ed5ce3819bd16b9683c16545a0eae8b37d
savaharu <82580739+savaharu@users.noreply.github.com>
2022-10-11 07:53:48 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+window.onload = () => {
+
+    document.getElementById('id_button2').onclick = () => {
+
+        window.location.replace('src/I-Filter回避用ツール(URL版)/index.html');
+
+    }
+    
+    document.getElementById('id_crgames').onclick = () => {
+    
+        window.location.replace('src/crazygames/index.html');
+    
+    }
+    
+    document.getElementById('id_replit').onclick = () => {
+    
+        window.location.replace('src/replit/index.html');
+    
+    }
+    
+    
+    document.getElementById('id_toge').onclick = () => {
+    
+        window.location.replace('src/TotallyGeometry(神)/index.html');
+    
+    }
+
+}
