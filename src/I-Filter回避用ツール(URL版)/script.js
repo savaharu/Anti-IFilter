51d4b8ef18dc6662fb9bb1141677381b5cb1020c
SVHR <82580739+savaharu@users.noreply.github.com>
2022-10-15 21:22:52 +0900

    Update script.js

--- src/I-Filter回避用ツール(URL版)/script.js
+++ src/I-Filter回避用ツール(URL版)/script.js
@@ -1,19 +1,3 @@
-const HEADERS_TO_STRIP_LOWERCASE = [
-  'content-security-policy',
-  'x-frame-options',
-];
-
-chrome.webRequest.onHeadersReceived.addListener(
-  details => ({
-    responseHeaders: details.responseHeaders.filter(header =>
-        !HEADERS_TO_STRIP_LOWERCASE.includes(header.name.toLowerCase()))
-  }),
-  {
-    urls: ['<all_urls>']
-  },
-  ['blocking', 'responseHeaders', 'extraHeaders']);
-
-
 document.getElementById('id_button').onclick = () => {
 
     let word = document.getElementById('id_text').value;
