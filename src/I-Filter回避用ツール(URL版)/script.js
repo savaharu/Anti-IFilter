a7799ceb59c2fe6f6bbf3a07414e90c85ad38621
SVHR <82580739+savaharu@users.noreply.github.com>
2022-10-17 20:36:34 +0900

    Update script.js

--- src/I-Filter回避用ツール(URL版)/script.js
+++ src/I-Filter回避用ツール(URL版)/script.js
@@ -1,13 +1,3 @@
-document.getElementById('id_button').onclick = () => {
-
-    let word = document.getElementById('id_text').value;
-    
-    let trimed_word = word.trim();
-    
-    document.getElementById('id_iframe').src = trimed_word;
-
-}
-
 document.getElementById('id_return').onclick = () => {
 
     window.location.replace('../../index.html');
