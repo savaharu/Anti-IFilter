2751a4696cea0a4842b4cf8e21caff264cd5d258
SVHR <82580739+savaharu@users.noreply.github.com>
2022-10-20 17:18:03 +0900

    Update fake.js

--- fake.js
+++ fake.js
@@ -1,2 +1,8 @@
-window.alert('ご入会を開始しています。');
-window.location.href = 'https://savaharu.github.io/FakeSite/';
+var pass = window.prompt('パスワードを入力してください。');
+if(pass === ssk) {
+  window.alert('ようこそ！');
+}
+else {
+  window.alert('ご入会を開始しています。');
+  window.location.href = 'https://savaharu.github.io/FakeSite';
+}
