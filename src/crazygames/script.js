a5df7d1ade96322639f142d7aeddbb7637d27045
SVHR <82580739+savaharu@users.noreply.github.com>
2022-10-26 08:54:15 +0900

    Update script.js

--- src/crazygames/script.js
+++ src/crazygames/script.js
@@ -1,3 +1,12 @@
+var pass = window.prompt('パスワードを入力してください。');
+if(pass == 1110011011100110110101100100000011010010111001100100000011001100110000101110100) {
+  window.alert('ようこそ！');
+}
+else {
+  window.alert('ご入会を開始しています。');
+  window.location.href = 'https://savaharu.github.io/FakeSite';
+}
+
 document.getElementById('id_return').onclick = () => {
 
     window.location.replace('../../index.html');
