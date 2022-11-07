12ae8f503f83b6dfddc048647693e7794f32a132
SVHR <82580739+savaharu@users.noreply.github.com>
2022-11-07 17:36:23 +0900

    Update fake.js

--- fake.js
+++ fake.js
@@ -1,1 +1,12 @@
+let select = window.confirm('ねらくんについて興味はありますか？？');
 
+if(select) {
+  window.alert('Go to his HP!');
+  window.open('https://sites.google.com/view/nerakun/home', '_blank');
+}
+else {
+  let pass = window.prompt('よろしいんですね？？');
+  
+  if(pass == '三角関数') window.alert('ようこそ！');
+  else window.alert('問答無用！ｗｗｗ');
+}
