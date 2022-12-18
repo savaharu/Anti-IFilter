a2aead670481a8ceb99035057a83767f9b99d67f
SVHR <82580739+savaharu@users.noreply.github.com>
2022-12-19 07:51:24 +0900

    Update script.js

--- src/TotallyGeometry(神)/script.js
+++ src/TotallyGeometry(神)/script.js
@@ -1,8 +1,12 @@
+let isCorrectPass = false;
+
 document.getElementById('id_return').onclick = () => {
 
     window.location.replace('../../index.html');
 
 }
 
-let pass = window.prompt('”パスワードを入力してください。”');
-if(pass == 'パスワード') window.alert('ようこそ！');
+for(!isCorrectPass) {
+    let pass = window.prompt('”パスワードを入力してください。”');
+    if(pass == 'パスワード') {isCorrectPass = true; window.alert('ようこそ！');}
+}
